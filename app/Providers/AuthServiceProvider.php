015176b6d8fd17467cbb144418327050053581d7
Jeka <jekaxv@gmail.com>
2018-01-03 09:51:39 +0200

    first gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+use App\User;
+
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -24,6 +26,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        
+        Gate::define('go', function(User $user){
+            
+          return $user->name === 'test';    
+        });
 
         //
     }
